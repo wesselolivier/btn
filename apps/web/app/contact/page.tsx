fdd7f9fbf526c65f05138dedaf99336df51715c6
wesselolivier <wolivier@retrorabbit.co.za>
2024-10-04 09:41:51 +0200

    Updated contact details

--- apps/web/app/contact/page.tsx
+++ apps/web/app/contact/page.tsx
@@ -1,7 +1,8 @@
 export default function Contact() {
     return (
       <div className="container">
-        <p>Contact Details.</p>
+        <p>+27 79 156 0880</p>
+        <p>email@transfer.com</p>
       </div>
     );
   }
