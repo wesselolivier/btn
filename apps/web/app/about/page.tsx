55f6ef378be1b497da0d0fc02db40d6c8555fbd9
wesselolivier <wolivier@retrorabbit.co.za>
2024-10-04 09:36:53 +0200

    Updated contact and about page

--- apps/web/app/about/page.tsx
+++ apps/web/app/about/page.tsx
@@ -1,7 +1,6 @@
 export default function About() {
   return (
     <div className="container">
-      <h1>About Transfer</h1>
       <p>Transfer makes it easy to quickly share your files without limitations, getting your ideas where they need to go.</p>
     </div>
   );
